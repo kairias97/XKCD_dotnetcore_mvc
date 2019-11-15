5c8ea83779dcb3382eaaf1471188d0f9ca3978dc
kairias97 <kevin.irias97@gmail.com>
2019-11-14 21:25:36 -0600

    removed old using namespace

--- XDCDDemo.Web/Controllers/HomeController.cs
+++ XDCDDemo.Web/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using XDCDDemo.Web.Models;
 
 namespace XDCDDemo.Web.Controllers
 {
