49cd28daa342272a9a50a99432c6b449f22795d6
kairias97 <kevin.irias97@gmail.com>
2019-11-14 21:24:22 -0600

    added view model to contain the metadata of an specific comic

---
+++ XDCDDemo.DTO/ViewModels/ComicDetailVM.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XDCDDemo.DTO.ViewModels
+{
+    public class ComicDetailVM
+    {
+        public string Month { get; set; }
+        public int Num { get; set; }
+        public string Link { get; set; }
+        public string Year { get; set; }
+        public string News { get; set; }
+        [JsonProperty(PropertyName = "safe_title")]
+        public string SafeTitle { get; set; }
+        public string Transcript { get; set; }
+        public string Alt { get; set; }
+        public string Img { get; set; }
+        public string Title { get; set; }
+        public string Day { get; set; }
+    }
+}
