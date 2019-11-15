f9eb3806fbffb259bc4cc1a1d4de58c2ec41a65f
kairias97 <kevin.irias97@gmail.com>
2019-11-14 23:12:30 -0600

    Added implementation of comic service

---
+++ XKCDDemo.Service/Interfaces/IComicService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XKCDDemo.DTO.ViewModels;
+
+namespace XKCDDemo.Service.Interfaces
+{
+    public interface IComicService
+    {
+        Task<DisplayedComicVM> GetComicOfTheDay();
+    }
+}
