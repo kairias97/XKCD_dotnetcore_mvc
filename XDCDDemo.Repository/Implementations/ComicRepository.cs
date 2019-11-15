1b09076ba0975bd5fd9ac5483435a9ac019486da
kairias97 <kevin.irias97@gmail.com>
2019-11-14 21:25:14 -0600

    added abstraction and implementation of comic repository with method to get comic of the day

---
+++ XDCDDemo.Repository/Implementations/ComicRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XDCDDemo.DTO.ViewModels;
+using XDCDDemo.Repository.Interfaces;
+
+namespace XDCDDemo.Repository.Implementations
+{
+    public class ComicRepository : IComicRepository
+    {
+        private readonly IXKCDApi _api;
+
+        [ActivatorUtilitiesConstructor]
+        public ComicRepository()
+        {
+            _api = RestService.For<IXKCDApi>("https://xkcd.com/");
+        }
+        //For future testing purposes
+        public ComicRepository(IXKCDApi api)
+        {
+            _api = api;
+        }
+        public async Task<ComicDetailVM> GetComicOfTheDay()
+        {
+            try
+            {
+                return await _api.GetComicOfTheDay();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ XDCDDemo.Repository/Interfaces/IComicRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XDCDDemo.DTO.ViewModels;
+
+namespace XDCDDemo.Repository.Interfaces
+{
+    public interface IComicRepository
+    {
+        Task<ComicDetailVM> GetComicOfTheDay();
+    }
+}
