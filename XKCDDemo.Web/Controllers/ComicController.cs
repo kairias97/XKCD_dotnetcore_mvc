14ced752c5437d5f2d17836ca8e288e4806675c6
kairias97 <kevin.irias97@gmail.com>
2019-11-16 22:57:15 -0600

    added comic controller with action to get and display comic info by id

---
+++ XKCDDemo.Web/Controllers/ComicController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XKCDDemo.Service.Interfaces;
+
+namespace XKCDDemo.Web.Controllers
+{
+    [Route("comic")]
+    public class ComicController : Controller
+    {
+        private readonly IComicService _comicService;
+
+        public ComicController(IComicService comicService)
+        {
+            _comicService = comicService;
+        }
+
+        [Route("{id}")]
+        public async Task<IActionResult> Detail([FromRoute]int id)
+        {
+            var detail = await _comicService.GetComicDetailById(id);
+            return View(detail);
+        }
+    }
+}
